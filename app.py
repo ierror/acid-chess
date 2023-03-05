c4a92acb26a7682f7d3a5afdcc894e72b6716808
Bernhard Janetzki <boerni@gmail.com>
2023-03-05 18:17:20 +0100

    fixed env set for bus error workaround (was set too late)

--- app.py
+++ app.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 import os
+
+# Workaround for bus error on M2
+# https://stackoverflow.com/questions/73072612/why-does-np-linalg-solve-raise-bus-error-when-running-on-its-own-thread-mac-m1/75317069#75317069
+os.environ["MKL_NUM_THREADS"] = "1"
+os.environ["NUMEXPR_NUM_THREADS"] = "1"
+os.environ["OMP_NUM_THREADS"] = "1"
+
 import signal
 import sys
 from multiprocessing import freeze_support
@@ -10,13 +17,8 @@ from qdarktheme.qtpy.QtWidgets import QApplication
 from acid.gui.main import MainWindow
 
 if __name__ == "__main__":
-    # Workaround for dbus error on M2
-    # https://stackoverflow.com/questions/73072612/why-does-np-linalg-solve-raise-bus-error-when-running-on-its-own-thread-mac-m1/75317069#75317069
-    os.environ["MKL_NUM_THREADS"] = "1"
-    os.environ["NUMEXPR_NUM_THREADS"] = "1"
-    os.environ["OMP_NUM_THREADS"] = "1"
-
     freeze_support()
+
     qdarktheme.enable_hi_dpi()
     app = QApplication(sys.argv)
     qdarktheme.setup_theme("dark", corner_shape="sharp")
